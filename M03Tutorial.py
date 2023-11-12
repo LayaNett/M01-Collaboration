989b4157491be1f8571f12a91dcf673a72a6b659
LayaNett <90700435+LayaNett@users.noreply.github.com>
2023-11-12 11:33:26 -0500

    Add files via upload

---
+++ M03Tutorial.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+#sorting class for inputs
+class Solution:
+    def sort012(self,arr,n):
+        arr.sort() #sort is a module, don't get confused Layla
+        return arr
+#Couldn't figure out how to print the solution to check if it works, but GeeksForGeeks says it works
+
+
+# In[ ]:
+
+
+class Solution:
+    def binarysearch(self, arr, n, k):
+        arr.index(n,k) #sort is a module, don't get confused Layla
+        return arr # in order of n, then looks at k
+#Also couldn't figure out how to print the solution to check if it works, but GeeksForGeeks says it works
+
